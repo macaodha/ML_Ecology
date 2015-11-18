db2790ea9b1f38b1f39b115b698bb79aab4a588d
macaodha <o.macaodha@cs.ucl.ac.uk>
2015-11-18 19:01:53 +0000

    updated basics

--- r_basics.R
+++ r_basics.R
@@ -19,6 +19,9 @@ help('function_name')
 Print text to screen:
 print('hi there')
 
+To print with a variable:
+print(paste("The number that comes after 1: ", 2.0))
+
 Assignment, use <- set variable (= only has local scope):
 aa <- 2
 bb <- 1
@@ -26,9 +29,8 @@ cc <- aa + bb
 
 Use the hash symbol to comment out a line:
 # This line will not do anything
-aa <- 2 # blaa
 
-Summarize object (i.e. variable):
+Summarize variable:
 summary(aa)
 
 Use c() to combine elements into a list:
@@ -43,13 +45,14 @@ length(my_vec)
 Functions:
 my_function <- function(text_to_print) { 
     print(text_to_print)
+    return(2)
 }
 
 To call:
-my_function('print this')
+ret_value <- my_function('print this')
 
 Load csv into DataFrame:
-my_data <- read.csv("iris.csv")
+my_data <- read.csv('data/flower_dataset.csv')
 
 Get the column names:
 names(my_data)
@@ -63,24 +66,19 @@ my_data[1, 1]
 my_data[1,]
 my_data[1:2,1:3]
 
-Access a column by name:
+Access a column by name (if there is a space in the name it will be replace by a .):
 my_data$Species
-If there is a space in the column name, it will be replaced with a ".", e.g. "Sepal Width" becomes "Sepal.Width"
 
 Simple plotting:
 plot(c(4,5,6))
-title("simple plot")
+title('simple plot')
 
 Or 2D:
 plot(c(4,5,6), c(1, 1, 2))
-
-Plot iris with different colors. pch is the format of the plotted circles - here filled circles:
-plot(flower_data$Petal_Length, flower_data$Petal_Width, col = flower_data$Species, pch=16)
 
 To save an open plot to disc:
 dev.print(pdf, 'filename.pdf')
 
 Generate random uniform numbers. Here, generate 10 random numbers between 0 and 1:
 runif(10, 0,1)
-
 
